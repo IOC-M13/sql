dce97dd868be7bff8dfb4078388282e3261dacb5
Oscar Membrilla Estorach <impactes@gmail.com>
2016-03-02 17:36:45 +0100

    He actualizado el nombre de la BDD, el nombre de las tablas y el nombre de algunas propiedades de las tablas. Ademas he creado el campo 'Color' como VARCHAR(6) para poner el color en Hexadecimal. Tambien he puesto el Script de insercion inicial acorde a la modificaciones del script de creacion. Por último, he agregado el usuario 'standard' con pass '1234' que tiene el mínimo de privilegios posibles

--- Script Initial Insert BDD.sql
+++ Script Initial Insert BDD.sql
@@ -4,8 +4,7 @@
 -- Disable foreign key checks
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS,FOREIGN_KEY_CHECKS=0;
 
-TRUNCATE `mydb`.`usuarios`;
-INSERT INTO `mydb`.`usuarios`(`dniUser`, `userName`, `realName`, `pass`, `admin`)
+TRUNCATE `dbChronoSchedule`.`Users`;
+INSERT INTO `dbChronoSchedule`.`Users`(`userDni`, `userName`, `realName`, `pass`, `admin`)
 VALUES ('39440859E', 'oscar', 'Oscar Membrilla Estorach', '1234', true), -- ID-1
-       ('39646585E', 'alex', 'Alejandro Oliván Alvarez', '1234', false), -- ID-2
-       ('59652528E', 'fran', 'Francisco Martínez García', '1234', false); -- ID-3
+       ('39646585E', 'alex', 'Alejandro Oliván Alvarez', '1234', false); -- ID-2
