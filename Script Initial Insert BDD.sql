1568af0623dd139cda20f3a9d0ec9b5f034931d1
Oscar Membrilla Estorach <impactes@gmail.com>
2016-03-04 20:56:02 +0100

    Cambiar nombre de algun campo de la BDD, que no estaba bien traducido. Restringir permisos del usuario 'standard' de la BD al minimo. Agregar datos iniciales en todas las tablas para poder empezar a trabajar con ellos, aunque solo sean pruebas.

--- Script Initial Insert BDD.sql
+++ Script Initial Insert BDD.sql
@@ -4,7 +4,21 @@
 -- Disable foreign key checks
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS,FOREIGN_KEY_CHECKS=0;
 
-TRUNCATE `dbChronoSchedule`.`Users`;
-INSERT INTO `dbChronoSchedule`.`Users`(`userDni`, `userName`, `realName`, `pass`, `admin`)
+TRUNCATE `dbChronoSchedule`.`users`;
+INSERT INTO `dbChronoSchedule`.`users`(`userDni`, `userName`, `realName`, `pass`, `admin`)
 VALUES ('39440859E', 'oscar', 'Oscar Membrilla Estorach', '1234', true), -- ID-1
        ('39646585E', 'alex', 'Alejandro Oliván Alvarez', '1234', false); -- ID-2
+
+
+TRUNCATE `dbChronoSchedule`.`shifts`;
+INSERT INTO `dbChronoSchedule`.`shifts`(`name`, `startTime`, `endTime`, `color`)
+VALUES ('Mañana', '07:00', '14:00', 'FFFFFF'), -- ID-1
+       ('Tarde', '14:00', '22:00', '000000'), -- ID-2
+       ('Noche', '22:00', '07:00', '00FF00'); -- ID-3
+       
+
+
+TRUNCATE `dbChronoSchedule`.`usershifts`;
+INSERT INTO `dbChronoSchedule`.`usershifts`(`idUser`, `idShift`, `date`)
+VALUES ('1', '1', '2016-03-15'),
+       ('1', '3', '2016-03-20');
