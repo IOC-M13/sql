ea8baef95dd44240a2f66c9c7265eab2610d7d69
Oscar Membrilla Estorach <impactes@gmail.com>
2016-03-11 18:58:46 +0100

    Hacer varias modificaciones al Script Initial Insert: Agregar un usuario no-admin, poner como admin al usuario alex, Agregar el turno 'Free' y traducir los nombres de los turnos al inglés, asignar 5 turnos a cada uno de los 3 usuarios que hay en la DB

--- Script Initial Insert BDD.sql
+++ Script Initial Insert BDD.sql
@@ -6,19 +6,34 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS,FOREIGN_KEY_CHECKS=0;
 
 TRUNCATE `dbChronoSchedule`.`users`;
 INSERT INTO `dbChronoSchedule`.`users`(`userDni`, `userName`, `realName`, `pass`, `admin`)
-VALUES ('39440859E', 'oscar', 'Oscar Membrilla Estorach', '1234', true), -- ID-1
-       ('39646585E', 'alex', 'Alejandro Oliván Alvarez', '1234', false); -- ID-2
+VALUES ('99440859D', 'armando', 'Armando Bronca Fuerte', '1234', false), -- ID-1
+       ('39440859E', 'oscar', 'Oscar Membrilla Estorach', '1234', true), -- ID-2
+       ('39646585H', 'alex', 'Alejandro Oliván Alvarez', '1234', true);  -- ID-3
 
 
 TRUNCATE `dbChronoSchedule`.`shifts`;
 INSERT INTO `dbChronoSchedule`.`shifts`(`name`, `startTime`, `endTime`, `color`)
-VALUES ('Mañana', '07:00', '14:00', 'FFFFFF'), -- ID-1
-       ('Tarde', '14:00', '22:00', '000000'), -- ID-2
-       ('Noche', '22:00', '07:00', '00FF00'); -- ID-3
+VALUES ('Free', '07:00', '14:00', '4FF943'),    -- ID-1
+	   ('Morning', '07:00', '14:00', 'E8A7A7'), -- ID-2
+       ('Evening', '14:00', '22:00', '4CB9FC'), -- ID-3
+       ('Night', '22:00', '07:00', '9B6EBC');   -- ID-4
        
 
 
 TRUNCATE `dbChronoSchedule`.`usershifts`;
 INSERT INTO `dbChronoSchedule`.`usershifts`(`idUser`, `idShift`, `date`)
-VALUES ('1', '1', '2016-03-15'),
-       ('1', '3', '2016-03-20');
+VALUES ('2', '1', '2016-03-15'),
+       ('2', '2', '2016-03-20'),
+       ('2', '3', '2016-03-02'),
+       ('2', '4', '2016-03-30'),
+       ('2', '1', '2016-03-01'),
+       ('3', '1', '2016-03-05'),
+       ('3', '2', '2016-03-09'),
+       ('3', '3', '2016-03-17'),
+       ('3', '4', '2016-03-18'),
+       ('3', '1', '2016-03-19'),
+       ('1', '1', '2016-03-21'),
+       ('1', '2', '2016-03-08'),
+       ('1', '3', '2016-03-29'),
+       ('1', '4', '2016-03-10'),
+       ('1', '1', '2016-03-11');
